e8ec87a01c0b54fe72fc5421c809ae7e38f41d11
Ankit Goel <goelankit@protonmail.com>
2018-09-28 12:40:57 +0530

    Create basic text website

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -5,9 +5,38 @@ struct WebsiteController: RouteCollection {
     
     func boot(router: Router) throws {
         router.get(use: indexHandler)
+        router.get("pods", Pod.parameter, use: podHandler)
     }
     
     func indexHandler(_ req: Request) throws -> Future<View> {
-        return try req.view().render("index")
+        return Pod.query(on: req)
+            .all()
+            .flatMap(to: View.self) { pods in
+                let acronymsData = pods.isEmpty ? nil : pods
+                let context = IndexContext(
+                    title: "Homepage",
+                    pods: acronymsData)
+                return try req.view().render("index", context)
+        }
     }
+    
+    func podHandler(_ req: Request) throws -> Future<View> {
+        return try req.parameters.next(Pod.self)
+            .flatMap(to: View.self) { pod in
+                let context = PodContext(
+                    title: pod.name,
+                    pod: pod)
+                return try req.view().render("pod", context)
+        }
+    }
+}
+
+struct IndexContext: Encodable {
+    let title: String
+    let pods: [Pod]?
+}
+
+struct PodContext: Encodable {
+    let title: String
+    let pod: Pod
 }
